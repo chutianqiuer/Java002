c66b2a84bbf5b1f19ac468cf4c96263b0e1dfdec
chutianqiu <X98kdHUXDAIgKylSYcA3RB+chutianqiu@noreply.cnb.cool>
2026-05-19 05:35:45 +0800

    feat: 接入 Nacos 服务注册中心
    - 删除 nacos-compose.yml（错误的外置 MySQL 路线）
    - 添加 start-services.sh 稳定启动脚本
    - 使用 standalone embedded Nacos

--- 2/mall-order/src/main/java/com/mall/order/MallOrderApplication.java
+++ 2/mall-order/src/main/java/com/mall/order/MallOrderApplication.java
@@ -3,9 +3,11 @@ package com.mall.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.mall.order", "com.mall.common"})
 @MapperScan("com.mall.order.mapper")
+@EnableDiscoveryClient
 public class MallOrderApplication {
 
     public static void main(String[] args) {
--- 2/mall-product/src/main/java/com/mall/product/MallProductApplication.java
+++ 2/mall-product/src/main/java/com/mall/product/MallProductApplication.java
@@ -3,9 +3,11 @@ package com.mall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.mall.product", "com.mall.common"})
 @MapperScan("com.mall.product.mapper")
+@EnableDiscoveryClient
 public class MallProductApplication {
 
     public static void main(String[] args) {
